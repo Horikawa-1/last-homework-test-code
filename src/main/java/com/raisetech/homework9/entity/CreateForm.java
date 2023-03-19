3e9babc2c50b5ce0650fd4930eb2d4a3221fdbbf
kaidou2 <rooftop@ymail.ne.jp>
2023-03-19 10:43:44 +0900

    こみっと

--- src/main/java/com/raisetech/homework9/entity/CreateForm.java
+++ src/main/java/com/raisetech/homework9/entity/CreateForm.java
@@ -3,12 +3,16 @@ package com.raisetech.homework9.entity;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 
 
 public class CreateForm {
 
+
+  public CreateForm(String name, int id) {
+    this.name = name;
+    this.id = id;
+  }
 
   public String getName() {
     return name;
--- src/main/java/com/raisetech/homework9/entity/UpdateForm.java
+++ src/main/java/com/raisetech/homework9/entity/UpdateForm.java
@@ -1,18 +1,20 @@
 package com.raisetech.homework9.entity;
 
-
+import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
-
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
 
 
 public class UpdateForm {
+
+  public UpdateForm(String name) {
+    this.name = name;
+  }
 
   public String getName() {
     return name;
   }
 
+  @NotBlank
   @Length(max = 20)
   private final String name;
 
